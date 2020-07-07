dc9f3c0839c5bcde42daf2dd1921220774fb969f
Kanika Sharma <kanika22@fb.com>
2020-07-07 13:55:14 -0400

    Changed app icon to instagram

--- app/src/main/java/com/example/instagram/LoginActivity.java
+++ app/src/main/java/com/example/instagram/LoginActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
-    /*private static final String TAG = "LoginActivity";
+    private static final String TAG = "LoginActivity";
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
@@ -36,5 +36,5 @@ public class LoginActivity extends AppCompatActivity {
 
     private void loginUser(String username, String password) {
         Log.i(TAG, "Attempting to login user "+ username);
-    }*/
+    }
 }
